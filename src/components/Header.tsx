fca55ea1cc5607dc3133b0850cb77c910fcd1f82
Sean Cesario <seancesario@mac.mynetworksettings.com>
2025-06-24 21:15:13 -0400

    remove image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import '../styles/styles.scss';
 function Header() {
     return (
         <section className="header">
-            <img src="" alt='image' />
             <div className="links">
                 <a>Search</a>
                 <a>About</a>
